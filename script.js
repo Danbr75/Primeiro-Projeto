178d4a6958f5955150cc1409b7e17a03f2e73e16
Danilo Brito dos Santos <141468447+Danbr75@users.noreply.github.com>
2023-11-20 13:23:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function start() {
+    alert("Função Start foi acionada!");
+    
+}
+
+function instr() {
+    window.location.href = 'http://127.0.0.1:5500/instrucao/instrucao.html';
+}
+
+function sair() {
+    alert("Função Sair foi acionada!");
+   
+}
